8af2aa27d5d04fb49d442f643ba186048738be92
Jhonny Bravo <sanfranceshika5@gmail.com>
2018-07-20 14:47:05 +0900

    new file: PSFirefoxCli.psm1

---
+++ PSFirefoxCli.psm1
@@ -0,0 +1,22 @@
+﻿<#
+.SYNOPSIS
+Firefox がインストールされているフォルダーのパスを返します。
+#>
+function Get-FirefoxPath(){
+    if(Test-Path "${env:ProgramFiles}/Mozilla Firefox"){
+        $FirefoxPath=Resolve-Path "${env:ProgramFiles}/Mozilla Firefox"
+    }elseif(Test-Path "${env:ProgramFiles(x86)}/Mozilla Firefox"){
+        $FirefoxPath=Resolve-Path "${env:ProgramFiles(x86)}/Mozilla Firefox"
+    }
+
+    return $FirefoxPath
+}
+
+<#
+.SYNOPSIS
+環境変数 Path へ Firefox がインストールされているフォルダーのパスを追加します。
+#>
+function Set-FirefoxPath(){
+    $FirefoxPath=Get-FirefoxPath
+    $env:Path="${env:Path}${FirefoxPath};"
+}
